6ff051a4cedcc066e10596f73f6164f529d36a31
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-08 12:55:54 +0530

    Finding maximum of 3 String values

--- Main.java
+++ Main.java
@@ -4,14 +4,17 @@ public class Main {
 	
 	public static void main(String args[]) {
 		String a1="Apple",b1="Peach",c1="Banana";
-		TestMaximum obj1=new TestMaximum(a1,b1,c1);
-		obj1.maximum();
+		TestMaximum obj1=new TestMaximum();
+		obj1.initString(a1, b1, c1);
+		obj1.findStringMax(a1, b1, c1);;
 		String a2="Peach",b2="Apple",c2="Banana";
-		TestMaximum obj2=new TestMaximum(a2,b2,c2);
-		obj2.maximum();
+		TestMaximum obj2=new TestMaximum();
+		obj1.initString(a2, b2, c2);
+		obj2.findStringMax(a2, b2, c2);;
 		String a3="Banana",b3="Peach",c3="Watermenol";
-		TestMaximum obj3=new TestMaximum(a3,b3,c3);
-		obj3.maximum();
+		TestMaximum obj3=new TestMaximum();
+		obj1.initString(a3, b3, c3);
+		obj3.findStringMax(a3, b3, c3);;
 	}
 
 }
