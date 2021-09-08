e1cb6f8e602e45049240f7cb8d18ae9ba80ffee8
jayesh200026 <jayeshkumar8660@gmail.com>
2021-09-08 14:47:32 +0530

    Added Generics type to reduce defining multiple methods

--- Main.java
+++ Main.java
@@ -4,17 +4,14 @@ public class Main {
 	
 	public static void main(String args[]) {
 		String a1="Apple",b1="Peach",c1="Banana";
-		TestMaximum obj1=new TestMaximum();
-		obj1.initString(a1, b1, c1);
-		obj1.findStringMax(a1, b1, c1);;
-		String a2="Peach",b2="Apple",c2="Banana";
-		TestMaximum obj2=new TestMaximum();
-		obj1.initString(a2, b2, c2);
-		obj2.findStringMax(a2, b2, c2);;
-		String a3="Banana",b3="Peach",c3="Watermenol";
-		TestMaximum obj3=new TestMaximum();
-		obj1.initString(a3, b3, c3);
-		obj3.findStringMax(a3, b3, c3);;
+		TestMaximum<String> obj1=new TestMaximum<>(a1, b1, c1);
+		obj1.findMax();
+		Float a2=10.7f,b2=50.8f,c2=30.3f;
+		TestMaximum<Float> obj2=new TestMaximum<>(a2, b2, c2);
+		obj2.findMax();
+		Integer a3=10,b3=50,c3=80;
+		TestMaximum<Integer> obj3=new TestMaximum<>(a3, b3, c3);
+		obj3.findMax();
 	}
 
 }
